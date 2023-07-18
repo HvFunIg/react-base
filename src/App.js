e5583b083c164a117c11f7c7a416f5c43f7f1c7b
Krotesk <igor_98_zukov@mail.ru>
2023-07-18 09:03:29 +0300

    Added Counter component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import Counter from "./components/Counter";
 
 function App() {
+  const [value,setValue] = useState(0);
+
+
   return (
     <div className="App">
-      
+      <Counter/>
+      <Counter/>
+      <Counter/>
     </div>
   );
 }
