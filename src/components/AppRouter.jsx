03ee6f3eee96324d860f588f42036250057b4f5a
Krotesk <igor_98_zukov@mail.ru>
2023-07-24 19:08:12 +0300

    Добавлено окно авторизации

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,13 +3,30 @@ import {Routes, Route, Navigate} from "react-router-dom"
 import About from "../pages/About";
 import Posts from "../pages/Posts";
 import PostIdPage from './PostIdPage';
+import Login from '../pages/Login';
+
 const AppRouter = () => {
+    const isAuth = false;
+
     return (
         <Routes>
-            <Route path="/*" element={<Navigate to="/posts"  />} />
-            <Route exact path="/posts" element={<Posts />} />
-            <Route exact path="/posts/:id" element={<PostIdPage />} />
-            <Route path="/about" element={<About />} />
+            {isAuth
+                ? 
+                    <>
+                        <Route path="/*" element={<Navigate to="/posts"  />} />
+                        <Route exact path="/posts" element={<Posts />} />
+                        <Route exact path="/posts/:id" element={<PostIdPage />} />
+                        <Route path="/about" element={<About />} />
+                    </>
+                :
+                    <>
+                        <Route path="/*" element={<Navigate to="/login"  />} />
+                        <Route path="/login" element={<Login />} />
+                    </>
+            }
+         
+  
+            
         </Routes>
     );
 };
